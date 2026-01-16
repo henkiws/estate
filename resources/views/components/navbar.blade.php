c0c75d02340ac76c2161f493fd76d7ac6fe292f0
henkiws <henki.subakti@gmail.com>
2026-01-16 19:16:03 +0700

    update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -122,12 +122,6 @@
                                         </svg>
                                         My Applications
                                     </a>
-                                    <a href="{{ route('user.enquiries.index') }}" class="flex items-center gap-3 px-4 py-2.5 text-sm text-plyform-dark hover:bg-plyform-mint transition-colors">
-                                        <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
-                                        </svg>
-                                        My Enquiries
-                                    </a>
                                 @endif
 
                                 <div class="my-2 border-t border-gray-200"></div>
